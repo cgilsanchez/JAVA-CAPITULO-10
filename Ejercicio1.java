343bf90dce3328d36307d4c8bd3eb4f32a4c2c16
cgilsanchez <cgilsanchez.02@gmail.com>
2024-01-29 10:13:26 +0100

    first commit

---
+++ Ejercicio1.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Ejercicio1{
+    public static void main(String[] args) {
+        ArrayList <String> nombreClase = new ArrayList<String>(); 
+
+        nombreClase.add("Antonio");
+        nombreClase.add("Dani");
+        nombreClase.add("Carlos");
+        nombreClase.add("Javi");
+        nombreClase.add("Pablo");
+        nombreClase.add("Adrian");
+
+        for(String nombres: nombreClase){
+            System.out.println(nombres);
+        }
+    }
+}
